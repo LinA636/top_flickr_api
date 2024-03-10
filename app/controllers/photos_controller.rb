dc3fe2611f9c03986e7b138eefa52f02d3d02452
Lina <117147821+LinA636@users.noreply.github.com>
2024-03-11 11:25:51 +1300

    add flickr API & show pictures of user_id

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,35 @@
 class PhotosController < ApplicationController
   def index
-       
+    console
+    url = "https://www.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=#{ENV["flickr_key"]}&user_id=#{params[:flickr_user_id]}&format=json&nojsoncallback=1"
+    response = HTTParty.get(url)
+    @response = JSON.parse(response.body)
+
+    if @response['stat'] == 'ok'
+      @photo_urls = getPhotoURLs(@response)
+    end
+  end
+
+  private
+
+  def photo_params
+      params.require(:photo).permit(:flickr_user_id)
+  end
+
+  def getPhotoURLs(response)
+    # array containing hashes with the photo details
+    photos_array = response['photos']['photo']
+    photo_urls = []
+    
+    photos_array.each do |photo_hash|
+      farm_id = photo_hash['farm']
+      server_id = photo_hash['server']
+      photo_id = photo_hash['id']
+      secret = photo_hash['secret']
+
+      photo_urls << "https://farm#{farm_id}.staticflickr.com/#{server_id}/#{photo_id}_#{secret}.jpg"
+    end
+    
+    photo_urls
   end
 end
